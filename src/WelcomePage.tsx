fe1441379c9f7d0753c87d31742ab6cfc6199632
Eric Michalsen <eric.michalsen@gmail.com>
2025-01-06 10:03:19 -0500

    Add .vercel to gitignore and refactor App component

---
+++ src/WelcomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './WelcomePage.css';
+
+const WelcomePage: React.FC = () => {
+  return (
+    <div className="welcome-container">
+      goodness me ...
+  );
+};
+
+export default WelcomePage;
